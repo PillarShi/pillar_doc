7e607cfd8b7a8c47a9a030bd73251dee1501491e
Pillar <pillar.shi3@gmail.com>
2024-11-24 21:55:04 +0800

    update RTOS/build_tool/menuconfig.py

--- RTOS/build_tool/menuconfig.py
+++ RTOS/build_tool/menuconfig.py
@@ -1,11 +1,12 @@
 import os
 import sys
 
-def main(config_name, config_h_name):
+def main(config_name, config_out_name):
     os.system("menuconfig")
     config = open(config_name, "r")
-    config_h = open(config_h_name, "w")
-    config_h_define = f"_{config_h_name.split('.')[0].upper()}_H_"
+    config_cmake = open(config_out_name+".cmake","w")
+    config_h = open(config_out_name+".h", "w")
+    config_h_define = f"_{config_out_name.upper()}_H_"
     config_h.write(f"#ifndef {config_h_define}\n#define {config_h_define}\n\n")
     for line in config:
         line = line.replace('\n', '').replace('\r', '')
@@ -14,32 +15,38 @@ def main(config_name, config_h_name):
             continue
         elif line_size == 1:
             config_h.write("\n")
+            config_cmake.write("\n")
             continue
 
         if line[0] == '#':
             if line[2] == 'C':
                 config_h.write(f"/* {line[9:]} */\n")
+                config_cmake.write(f"# {line[9:]}\n")
             else:
                 config_h.write(f"/* {line[2:]} */\n")
+                config_cmake.write(f"# {line[2:]}\n")
         else:
             line_message = line.split('=')
             if line_message[1] == "y":
                 config_h.write(f"#define {line_message[0][7:]}\n")
+                config_cmake.write(f"set({line_message[0][7:]} y)\n")
             else:
                 config_h.write(f"#define {line_message[0][7:]} {line_message[1]}\n")
+                config_cmake.write(f"set({line_message[0][7:]} {line_message[1]})\n")
 
     config_h.write(f"\n#endif /* {config_h_define} */\n")
     config_h.close()
+    config_cmake.close()
     config.close()
 
 if __name__ == "__main__":
     argc = len(sys.argv)
     if argc == 1:
-        main(".config", "config.h")
+        main(".config", "config")
     elif argc == 2:
-        main(sys.argv[1], "config.h")
+        main(sys.argv[1], "config")
     elif argc == 3:
         main(sys.argv[1], sys.argv[2])
     else :
-        print(f"python {sys.argv[0]} <.config> <config.h>")
+        print(f"python {sys.argv[0]} <.config input> <output name>")
         exit(1)
